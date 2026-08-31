211725e723332829d94575250c345653560ef98f
varun kumar <varunkumarkanumuri@gmail.com>
2026-08-31 10:36:57 +0530

    to create the Home.css File

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
+import "./Home.css";
 function Home() {
   return (
     <div className="home">
-        <h1>Build Your Future With Us</h1>
+        <div className="hero">
+            <h1>Build Your Future With Us</h1>
 
-        <p>
-          We create simple, modern and powerful digital solutions
-          to help your business grow.
-        </p>
+                <p>
+                We create simple, modern and powerful digital solutions
+                to help your business grow.
+                </p>
 
         <button>Get Started</button>
+        </div>
+        
     </div>
   );
 }
