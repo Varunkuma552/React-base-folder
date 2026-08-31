8ea4979f8dec9b65c20a6731ec581166e404705b
varun kumar <varunkumarkanumuri@gmail.com>
2026-08-31 11:42:43 +0530

    to create the About section with jsx and css file within the page folder

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './pages/Home.jsx'
 import Services from './pages/Services.jsx'
+import About from './pages/About.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Home />
     <Services/>
+    <About/>
   </StrictMode>,
 )
---
+++ src/pages/About.jsx
@@ -0,0 +1,31 @@
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about">
+      <section className="about-section">
+        <div className="about-content">
+          <h1>About Us</h1>
+
+          <p>
+            We are a passionate team focused on creating simple,
+            modern and effective digital experiences.
+          </p>
+
+          <p>
+            Our goal is to help businesses grow by using technology,
+            creativity and thoughtful design.
+          </p>
+
+          <button>Learn More</button>
+        </div>
+
+        <div className="about-image">
+          <div>Our Team</div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default About;
