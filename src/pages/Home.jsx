3579f94a9106bd6f01dcc0981751e4f8f053c535
varun kumar <varunkumarkanumuri@gmail.com>
2026-08-31 09:48:31 +0530

    create the Home.jsx file

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+function Home() {
+  return (
+    <div className="home">
+      <section className="hero">
+        <h1>Build Your Future With Us</h1>
+
+        <p>
+          We create simple, modern and powerful digital solutions
+          to help your business grow.
+        </p>
+
+        <button>Get Started</button>
+      </section>
+    </div>
+  );
+}
+
+export default Home;
