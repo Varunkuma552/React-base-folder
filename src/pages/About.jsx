9afea06b0f2d7b1f02077ca90d573af1ccaf6f5f
varun kumar <varunkumarkanumuri@gmail.com>
2026-09-02 16:35:14 +0530

    fix: To adjust the space

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,6 @@ function About() {
       <section className="about-section">
         <div className="about-content">
           <h1>About Us</h1>
-
           <p>
             We are a passionate team focused on creating simple,
             modern and effective digital experience.
@@ -16,7 +15,6 @@ function About() {
             Our goal is to help businesses grow by using technology,
             creativity and thoughtful design.
           </p>
-
           <button>Learn More</button>
         </div>
 
